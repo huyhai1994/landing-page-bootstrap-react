deb5192fe8c20d0d1bf135d94e143ab2898033ee
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-21 19:49:18 +0700

    finished TopBar(NavBar)

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './TopBar.css'
-import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
+import {Button, Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
 import Logo from '../../assets/logo.png'
 
 const TopBar = () => {
     return (<div>
-        <Navbar expand="lg" className="bg-body-tertiary">
+        <Navbar expand="lg" className="bg-transparent shadow-sm ">
             <Container>
                 <Navbar.Brand href="#home"> <img src={Logo} alt="Logo" className="logo"
                                                  style={{
@@ -13,9 +13,21 @@ const TopBar = () => {
                                                  }}/></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">
+                    <Nav className="ms-auto text-center d-flex justify-content-center align-items-center">
+                        <Nav.Link href="#home" className='text-secondary fw-bold'>
                             Home
+                        </Nav.Link>
+                        <Nav.Link href="#home" className='text-secondary fw-bold'>
+                            About Us
+                        </Nav.Link>
+                        <Nav.Link href="#home" className='text-secondary fw-bold'>
+                            Contact
+                        </Nav.Link>
+                        <Nav.Link href="#home" className='text-secondary fw-bold'>
+                            Testimonials
+                        </Nav.Link>
+                        <Nav.Link href="#home" className='text-secondary fw-bold text-decoration-none border-0'>
+                            <Button size='sm' className=' bg_login fw-bold text-decoration-none'>Login</Button>
                         </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
