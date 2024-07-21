5c4f8b42055cd3dbb7e6588b6af1174fd0e54a18
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-22 05:48:13 +0700

    updating Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './Header.css'
+import {Col, Container, Row} from "react-bootstrap";
 
 const Header = () => {
-    return (
-        <div>Header</div>
+    return (<div className='mt-5'>
+            <Container>
+                <Row>
+                    <Col md={6}>
+                        <div className="head-left">
+                        <span className="head-subtitle">
+                            Healthy & Tasty Food
+                        </span>
+                            <div className="w-100 mt-2 head-title border">
+                                <h1 style={{color: '#FB9431'}}>Archive Optimal </h1>
+                                <h1 style={{color: '#3D3F42'}}>Wellness with our Nutrients </h1>
+                                <h1 style={{color: '#3D3F42'}}> Meal Plan</h1>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+
     )
 }
 export default Header
